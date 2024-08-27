8bd9f0d92fbc909013ceeaec26d349ff58fe9eb0
Viktor Holta <viktor.holta@outlook.com>
2024-08-27 21:32:21 +0200

    Use different URL for development vs production

--- medlem/src/context/MembershipContext.js
+++ medlem/src/context/MembershipContext.js
@@ -6,19 +6,22 @@ export const MembershipProvider = ({ children }) => {
   const [status, setStatus] = useState({})
   const addMember = (member) => {
     const newMember = { ...member };
+    const url = (process.env.NODE_ENV === 'development')
+      ? "http://localhost:8000/members/"
+      : "https://lithehax-medlem-9abc9f434ec7.herokuapp.com/members/";
     console.log(newMember)
-    fetch("https://lithehax-medlem-9abc9f434ec7.herokuapp.com/members/create/", {
+    fetch(url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(newMember),
     }).then(res => {
-      if(res.status === 200) {
-        setStatus({message: "Member request sent successfully!", color: "green"})
+      if (res.status === 200) {
+        setStatus({ message: "Member request sent successfully!", color: "green" })
       }
-      else if(res.status === 409) {
-        setStatus({message: `Membership request with email ${newMember.email} already exists!`, color: "red"})
+      else if (res.status === 409) {
+        setStatus({ message: `Membership request with email ${newMember.email} already exists!`, color: "red" })
       }
     }).catch(err => {
       setStatus({ message: `Error: ${err.message}`, color: "red" });
