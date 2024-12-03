e00b9dd554d66f25aa8100b937a66decfd9dcb7c
Viktor Holta <viktor.holta@outlook.com>
2024-12-04 00:08:42 +0100

    Add sections and text to pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
-<p>This is the home page.</p>
+<script lang="ts">
+    import Section from "$lib/components/Section.svelte";
+</script>
+
+<Section>
+    <h1>About us</h1>
+    <p>
+        LiTHe Hax is an independent student association at Linköping University whose
+        purpose is to promote and strengthen knowledge in ethical hacking and cybersecurity.
+        The association is intended to participate in and organize hacking competitions.
+        Anyone can be a member and participate in LiTHe Hax's activities. The association
+        was founded on March 29, 2024.
+    </p>
+
+    <h2>Contact</h2>
+    <!-- TODO: add contact cards -->
+
+    <h2>Organization details</h2>
+    <p><b>Organization Number:</b> 802547-1767</p>
+    <p><b>Adress:</b> LiTHe Hax, Kårallen, Universitetet, 581 83 Linköping</p>
+
+    <h3>Documents</h3>
+    <!-- TODO: add documents -->
+</Section>
--- src/routes/new-member/+page.svelte
+++ src/routes/new-member/+page.svelte
@@ -1,1 +1,14 @@
-<p>This is the new-member page.</p>
+<script lang="ts">
+    import Section from "$lib/components/Section.svelte";
+</script>
+
+<Section isThin>
+    <h1>Become a member</h1>
+    <p>
+        Join LiTHe Hax and become a member! As a member, you'll gain access to exclusive
+        events, resources, and updates about our organization's activities. Once your
+        membership is approved by our board, we will contact you with further details.
+    </p>
+
+    <!-- TODO: add membership form -->
+</Section>
