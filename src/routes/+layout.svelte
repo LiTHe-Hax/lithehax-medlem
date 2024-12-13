a6352eaf8b99f8ad0e201932ac27f61536560889
Viktor Holta <viktor.holta@outlook.com>
2024-12-13 19:33:05 +0100

    Add nav button leading to christmas 2024 page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,6 +9,7 @@
     <nav>
         <NavButton href={`${base}/`} label="Home" />
         <NavButton href={`${base}/new-member`} label="Member" />
+        <NavButton href={`${base}/christmas-2024`} label="Christmas 2024" />
     </nav>
 </header>
 
