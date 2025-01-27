c8f5bf5b62d9c31a3a4e6d33a149516668268174
Wilmer Segerstedt <77577640+wilmersegerstedt@users.noreply.github.com>
2025-01-27 18:11:38 +0100

    replace placeholder image on ctf event page

--- src/routes/ctf/+page.svelte
+++ src/routes/ctf/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import Section from "$lib/components/Section.svelte";
-    import cyberRoom from "$lib/images/cybersecurity-room-placeholder.jpg"
+    import cyberRoom from "$lib/images/ctf/cyber-room.jpg"
 </script>
 
 <svelte:head>
@@ -18,15 +18,19 @@
     <p>
         Sign up details will be available soon. Stay tuned for updates!
     </p>
+    <p><strong>Date:</strong> March 8th, 2025</p>
     <p>
         <strong>Location:</strong>
         <a href="https://link.mazemap.com/0PapbQ2Z" target="_blank" rel="noopener noreferrer">
             Linköping University, Sweden
         </a>
     </p>
-    <p><strong>Date:</strong> March 8th, 2025</p>
     <img src={cyberRoom} alt="Cybersecurity Room" />
 </Section>
 
 <style>
+    img {
+        max-width: 100%;
+        height: auto;
+    }
 </style>
