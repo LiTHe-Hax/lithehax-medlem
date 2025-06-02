e51f4ba79b9ace454b6cadc591e525f15026a71b
wind1s <44732263+wind1s@users.noreply.github.com>
2025-06-02 16:25:20 +0200

    Add canonical meta tag to base layout

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,7 @@
 // This is needed, since the website is using adapter-static
 export const prerender = true;
+
+export function load({ url }) {
+  const canonicalHref = `${__BASE_URL__}${url.pathname}`;
+  return { canonicalHref };
+}
