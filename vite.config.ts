51acc5c8971ef0aaa742421be7a0d6727b549aa9
Viktor Holta <viktor.holta@outlook.com>
2025-06-14 16:10:33 +0200

    Remove overridden base option from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
-	base: '/',
 	define: {
       '__BASE_URL__': JSON.stringify('https://lithehax.se'),
     },
